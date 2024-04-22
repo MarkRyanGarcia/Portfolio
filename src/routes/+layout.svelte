1cb8bb78a8c75f192d44f427d582a32db419d5dc
Mark Garcia <MarkRGarcia@csu.fullerton.edu>
2024-04-22 11:58:53 -0700

    setup tailwindcss

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,5 @@
+<script>
+    import "/static/global.css"
+</script>
+
+<slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,5 @@
-<script lang="ts">
 
-</script>
 
-<h1 align="center">Hello! I'm Mark (:</h1>
-<h3 align="center">I like to code :)</h3>
+<h1 class="text-center text-4xl font-bold">Hello! I'm Mark (:</h1>
+<h3 class="text-center text-2xl">I like to code :)</h3>
 This is where I'll post my projects eventually lol
