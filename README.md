8a63c025a373781731357a391a83378f8121b0a3
Mark Garcia <MarkRGarcia@csu.fullerton.edu>
2024-04-21 19:08:46 -0700

    Created Svelte Skeleton

---
+++ README.md
@@ -0,0 +1,3 @@
+### Repo for upcoming Portfolio Site
+
+I am new to this idk what im doing tbh
---
+++ src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+
+</script>
+
+<h1>Mark Ryan Garcia</h1>
+<p>I like to code :)</p>
+This is where I'll post my projects eventually lol
