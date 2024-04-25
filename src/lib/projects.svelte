75db6201d33e10ee8272aa59688fde397142901c
Mark Garcia <MarkRGarcia@csu.fullerton.edu>
2024-04-25 15:33:35 -0700

    idk what im doing but im fixing the project list

--- src/lib/projects.svelte
+++ src/lib/projects.svelte
@@ -4,12 +4,12 @@
     </h1>
 </div>
 
-<div>
+<div class="project">
     <h3 class="text-2xl font-bold text-gray-950">
         Marktris
     </h3>
-    <p class="text-black project">
-        A Tetris clone made in godot. It includes many in-game features to make the game as faithful to modern tetris as possible. This includes 7-bag, wall-kicks, the Super Rotation System, and will soon include more.
+    <p class="text-black">
+        A Tetris clone made in godot. It includes many in-game features to make the game as faithful to modern tetris as possible. This includes 7-bag, wall-kicks, the Super Rotation System, and will soon include more. Web Deployment coming soon...
     </p>
     <img src="/TitleScreen.png" alt="Tetris Title Screen" width="300">
     <img src="/marktrisscreenshot2.png" alt="Tetris Project Screenshot" width="300">
@@ -18,8 +18,10 @@
 
 <style>
     .project {
+        /* display: flex; */
         width: 500px;
         background-color: rgba(84,58,183,.2);
         border-radius: 10px;
+        justify-content: center;
     }
 </style>
