c4d37ac95bfef2c068ab9a60fb9c81704fe6e087
MarkRyanGarcia <MarkRGarcia@csu.fullerton.edu>
2025-06-17 23:58:54 -0700

    more projects page updates

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -21,14 +21,25 @@ export default function Projects() {
             description: 'As part of a beginner-friendly Unity workshop I led, I developed a 2D platformer game inspired by Doodle Jump to teach the fundamentals of game development using Unity and C#.',
             image_path: '/public/ProjectImages/doodlejump1.png',
             links: {
-                Deployment: 'https://monkeytype.com',
                 Github: 'https://github.com/MarkRyanGarcia/Doodle-Jump-Gamedev-SP25'
             }
         },
         {
             title: 'Marktris',
-            description: 'A Tetris clone made in godot. It includes many in-game features to make the game as faithful to modern tetris as possible. This includes 7-bag, wall-kicks, and the Super Rotation System.',
+            description: 'A Tetris clone made in godot. It includes many in-game features to make the game as faithful to modern tetris as possible. This includes 7-bag, wall-kicks, and the Super Rotation System. Warning: Use Chrome for best experience.',
             image_path: '/public/ProjectImages/marktris2.png',
+            links: {
+                Deployment: 'https://marktris.markg.dev/',
+            }
+        },
+        {
+            title: 'Portolio Site',
+            description: "Hey what's this doing here?",
+            image_path: '/public/ProjectImages/portfolio.png',
+            links: {
+                Deployment: 'https://markg.dev/',
+                Github: 'https://github.com/MarkRyanGarcia/Portfolio'
+            }
         },
     ];
 
@@ -41,7 +52,7 @@ export default function Projects() {
                         const isOdd = project_index % 2 === 0;
                         console.log('links', project.links)
                         return (
-                            <Stack sx={{ alignItems: 'center', width: '100%', backgroundColor: `${isOdd ? '' : 'rgba(5, 147, 234, .25)'}` }}>
+                            <Stack sx={{ alignItems: 'center', width: '100%', backgroundColor: `${isOdd ? '' : 'rgba(200, 200, 200, .25)'}` }}>
                                 <Stack sx={{ width: '50vw', py: 1 }}>
                                     <Stack direction={`row${isOdd ? '-reverse' : ''}`} sx={{ alignItems: 'center', justifyContent: 'center' }} spacing={2}>
                                         <Stack spacing={1}>
@@ -50,7 +61,7 @@ export default function Projects() {
                                             {project.links && (
                                                 <Stack direction={'row'} sx={{ justifyContent: 'center' }} spacing={2}>
                                                     {project.links.Deployment && (
-                                                        <Button variant="contained" color='info' href={project.links.Deployment} target="_blank">Site</Button>
+                                                        <Button variant="contained" color='info' href={project.links.Deployment} target="_blank">Visit Site</Button>
                                                     )}
                                                     {project.links.Github && (
                                                         <Button variant="contained" color='info' href={project.links.Github} target="_blank">Github</Button>
@@ -74,6 +85,16 @@ export default function Projects() {
                                         <Typography variant='h6'>{project.title}</Typography>
                                         <Typography variant='caption' sx={{}}>{project.description}</Typography>
                                         <img src={project.image_path} alt={`${project.title} image`} style={{ width: 250, borderRadius: 5 }} />
+                                        {project.links && (
+                                            <Stack direction={'row'} sx={{ justifyContent: 'center' }} spacing={2}>
+                                                {project.links.Deployment && (
+                                                    <Button variant="contained" color='info' href={project.links.Deployment} target="_blank">Visit Site</Button>
+                                                )}
+                                                {project.links.Github && (
+                                                    <Button variant="contained" color='info' href={project.links.Github} target="_blank">Github</Button>
+                                                )}
+                                            </Stack>
+                                        )}
                                     </Stack>
                                 </Stack>
                             </Stack>
